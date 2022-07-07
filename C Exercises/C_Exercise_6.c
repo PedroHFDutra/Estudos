baa7bfd1700c6a9cb00cd7282bcc87f2189bf72e
Pedro Henrique <106541103+PedroHFDutra@users.noreply.github.com>
2022-07-07 01:20:13 -0300

    Update C_Exercise_6.c

--- C Exercises/C_Exercise_6.c
+++ C Exercises/C_Exercise_6.c
@@ -1,9 +1,7 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <math.h>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
 int main() {
 	
